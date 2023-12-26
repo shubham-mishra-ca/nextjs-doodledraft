ee2832be9c1ed6c65c2213965f7763d690df4360
Shubham Mishra <smishra.on.ca@gmail.com>
2023-12-25 21:52:09 -0500

    Added global hover and active styles to Menu component using tailwindcss/nesting and autoprefixer plugins

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,24 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
+
+import styles from './index.module.css'
 const Menu = () => {
     return (
-        <div>
-            <div>
-                    <FontAwesomeIcon icon={faPencil} />
+        <div className={styles.menuContainer}>
+            <div className={styles.iconWrapper}>
+                    <FontAwesomeIcon icon={faPencil} className={styles.icon} />
             </div>
-            <div>
-                    <FontAwesomeIcon icon={faEraser} />
+            <div className={styles.iconWrapper}>
+                    <FontAwesomeIcon icon={faEraser} className={styles.icon} />
             </div>
-            <div>
-                    <FontAwesomeIcon icon={faRotateLeft} />
+            <div className={styles.iconWrapper}>
+                    <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
             </div>
-            <div>
-                    <FontAwesomeIcon icon={faRotateRight} />
+            <div className={styles.iconWrapper}>
+                    <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
             </div>
-            <div>
-                    <FontAwesomeIcon icon={faFileArrowDown} />
+            <div className={styles.iconWrapper}>
+                    <FontAwesomeIcon icon={faFileArrowDown}className={styles.icon} />
             </div>
         </div>
     )
