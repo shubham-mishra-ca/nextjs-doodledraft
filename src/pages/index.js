c9b09406f07f2f39097b6d96daa8d4a374bcd6ce
Shubham Mishra <smishra.on.ca@gmail.com>
2023-12-26 00:07:43 -0500

    Added Toolbox UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import Menu from '../components/Menu';
+import Toolbox from '@/components/Toolbox';
 
 export default function Home() {
   return (
+    <>
     <Menu />
+    <Toolbox />
+    </>
+
   )
 }
